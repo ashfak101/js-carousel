e8cfff31f728dc815fa231132aa287d10fa1b036
Staff Asia <staffasia@Staffs-iMac-2.local>
2022-01-25 14:49:38 +0600

    Normal Carousel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('hello from js')
 
 
 const sliders =document.querySelector(".sliders");
-const leftArrow = document.getElementById('arrow-l');
+const leftArrow = document.getElementById("arrow-l");
 const  rightArrow= document.getElementById("arrow-r");
 const slider = Array.from(document.getElementsByClassName("slider"))
  let sliderIndex = 0;
@@ -12,7 +12,18 @@ const slider = Array.from(document.getElementsByClassName("slider"))
 // const handleArrow=()=>{
 //     console.log('clicked')
 // } 
-rightArrow.addEventListener("click", ()=> {
+ setInterval(()=> {
     sliderIndex = (sliderIndex < 5 )? sliderIndex + 1: 0;
      sliders.style.transform = 'translate(' +( sliderIndex)* -15 +'%)';
-  });
+  },3000);
+
+  leftArrow.addEventListener('click',()=>{
+    sliderIndex = (sliderIndex > 0 )? sliderIndex - 1: 0;
+    sliders.style.transform = 'translate(' +( sliderIndex)* -15 +'%)';
+})
+
+
+rightArrow.addEventListener('click',()=>{
+    sliderIndex = (sliderIndex < 5 )? sliderIndex + 1: 5;
+    sliders.style.transform = 'translate(' +( sliderIndex)* -15 +'%)';
+})
